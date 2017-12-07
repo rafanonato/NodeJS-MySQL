f4097c9f3759f33ee3b5581896e4d93fc9415529
Rafa Nonato <rf.nonato@gmail.com>
2017-12-07 16:18:34 -0200

    Commit Inicial

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();         
+const bodyParser = require('body-parser');
+const port = 8080; //porta padrão
+const mysql = require('mysql');
+
+//configurando o body parser para pegar POSTS mais tarde
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//definindo as rotas
+const router = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+app.use('/', router);
+
+//inicia o servidor
+app.listen(port);
+console.log('API funcionando!');
+
+function execSQLQuery(sqlQry, res){
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    port     : 3306,
+    user     : 'root',
+    password : '',
+    database : 'test'
+  });
+ 
+  connection.query(sqlQry, function(error, results, fields){
+      if(error) 
+        res.json(error);
+      else
+        res.json(results);
+      connection.end();
+      console.log('executou!');
+  });
+}
+
+router.get('/clientes', (req, res) =>{
+    execSQLQuery('SELECT * FROM Clientes', res);
+})
+
+router.get('/clientes/:id?', (req, res) =>{
+    let filter = '';
+    if(req.params.id) filter = ' WHERE ID=' + parseInt(req.params.id);
+    execSQLQuery('SELECT * FROM Clientes' + filter, res);
+})
+
+router.delete('/clientes/:id', (req, res) =>{
+    execSQLQuery('DELETE FROM Clientes WHERE ID=' + parseInt(req.params.id), res);
+})
+
+router.post('/clientes', (req, res) =>{
+  const nome = req.body.nome.substring(0,150);
+  const cpf = req.body.cpf.substring(0,11);
+  execSQLQuery(`INSERT INTO Clientes(Nome, CPF) VALUES('${nome}','${cpf}')`, res);
+});
